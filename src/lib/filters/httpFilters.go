e7980baeb1b777701e4f09ebce7750b38917766a
Harkishen-Singh <harkishensingh@hotmail.com>
2019-09-09 22:12:54 +0530

    http base init

---
+++ src/lib/filters/httpFilters.go
@@ -0,0 +1,18 @@
+package filters
+
+import (
+	"strings"
+)
+
+// HTTPPingFilter filters the illegal characters that may panic the ping 
+// subprocess running from the terminal
+func HTTPPingFilter(s *string) *string {
+	*s = strings.Replace(*s, "http://", "", -1)
+	*s = strings.Replace(*s, "https://", "", -1)
+	*s = strings.Replace(*s, "https:", "", -1)
+	*s = strings.Replace(*s, "http:", "", -1)
+	*s = strings.Replace(*s, "/", "", -1)
+	*s = strings.Replace(*s, "www.", "", -1)
+	*s = strings.Replace(*s, ":", "", -1)
+	return s
+}
---
+++ src/lib/utils/http.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/filters"
+)
+
+// cli command base
+const (
+	CmdPingBasedOnPacketsNumber = "ping"
+)
+
+// CLIPing works as an *independent subroutine*, for ping operations with the external networks
+// Takes in a pointer channel in the last params inorder to implement subroutines since the ping 
+// operations might take time thereby avoiding delay in the other operations
+// Use of pointers necessary since the data received is of large size, thereby slowing the traditional
+// method of variables, as using variables require the time involved in loading into and out from cpu registers.
+// Specifying addresses directly speeds the entire process manyfolds.
+func CLIPing(url *string, packets int, cliPingChannel chan *string) {
+	fmt.Println("called "+*url)
+	url = filters.HTTPPingFilter(url)
+	cmd, err := exec.Command(CmdPingBasedOnPacketsNumber, "-c", strconv.Itoa(packets), *url).Output()
+	if err != nil {
+		panic(err)
+	}
+
+	cmdStr := string(cmd)
+	fmt.Println(cmdStr)
+	cliPingChannel <- &cmdStr
+}
