d2310f8622dafbf1777d5a638bf07ac40875d914
Jayashree-panda <33364652+Jayashree-panda@users.noreply.github.com>
2019-09-26 16:45:54 +0530

    notification module fixes#16 (#30)
    * notify module
    
    * bug fixed
    
    Signed-off-by: Jayashree-panda <jayashreepanda12345@gmail.com>
    
    * bug fixed
    
    Signed-off-by: Jayashree-panda <jayashreepanda12345@gmail.com>
    
    * fix: change method names
    
    Signed-off-by: Jayashree-panda <jayashreepanda12345@gmail.com>

---
+++ src/lib/modules/notification/notify_test.go
@@ -0,0 +1,31 @@
+package notify
+
+import (
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestSendNotification(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	substr := "/bench-routes/"
+	c := strings.Index(dir, substr)
+	dir = dir[:c+len(substr)] + "assets/icon.png"
+	var n = NotificationData{
+		title:       "hi",
+		description: "hello",
+		icon:        dir,
+		urgency:     "critical",
+		time:        5000,
+	}
+	n.NotifyBasic()
+	n.NotifyWithTimeSpan()
+	n.NotifyWithImageIcon()
+	n.NotifyWithUrgencyLevel()
+	n.NotifyWithImageAndTimeSpan()
+	n.NotifyWithUrgencyLevelAndTimeSpan()
+}
