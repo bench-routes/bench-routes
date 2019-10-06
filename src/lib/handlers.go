796b6a29b0041be3e0352a8baa4f4daf6d26b93b
flyznex <thuanpt.dev@gmail.com>
2019-10-06 13:40:27 +0700

    Simplify handlers.go
    Signed-off-by: flyznex <thuanpt.dev@gmail.com>

--- src/lib/handlers.go
+++ src/lib/handlers.go
@@ -1,13 +1,9 @@
 package lib
 
 import (
-	"github.com/zairza-cetb/bench-routes/src/lib/filters"
-	"github.com/zairza-cetb/bench-routes/src/lib/modules/ping"
-	"github.com/zairza-cetb/bench-routes/src/lib/utils"
-	"github.com/zairza-cetb/bench-routes/tsdb"
 	"log"
-	"sync"
-	"time"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/utils"
 )
 
 const (
@@ -38,77 +34,7 @@ func HandlerPingGeneral(signal string) bool {
 
 	switch signal {
 	case "start":
-		if pingServiceState == "passive" {
-
-			Configuration.Config.UtilsConf.ServicesSignal.Ping = "active"
-			_, e := Configuration.Write()
-			if e != nil {
-				panic(e)
-			}
-			go func() {
-				pingConfig := Configuration.Config.Routes
-				pingInterval := getInterval(Configuration.Config.Interval, "ping")
-				if pingInterval == (testInterval{}) {
-					log.Fatalf("interval not found in configuration file for ping")
-					return
-				}
-
-				urlStack := make([]string, 0)
-				urlHashMap := make(map[string]string)
-				for _, route := range pingConfig {
-					url := route.URL
-					exists := false
-
-					// maintain urls uniquely
-					for _, e := range urlStack {
-						if e == url {
-							exists = true
-						}
-					}
-					if !exists {
-						urlStack = append(urlStack, *filters.HTTPPingFilter(&url))
-						urlHashMap[url] = utils.GetHash(url)
-					}
-				}
-				i := 0
-				for {
-					i++
-					Configuration = Configuration.Refresh()
-
-					switch Configuration.Config.UtilsConf.ServicesSignal.Ping {
-					case "active":
-						var wg sync.WaitGroup
-						wg.Add(len(urlStack))
-						for u := range urlStack {
-							go ping.HandlePing(tsdb.GlobalPingChain, &urlStack[u], 10, urlHashMap[urlStack[u]], &wg, false)
-						}
-
-						wg.Wait()
-					case "passive":
-						// terminate the goroutine
-						log.Printf("terminating ping goroutine\n")
-						return
-					default:
-						log.Fatalf("invalid service-state value of ping\n")
-						return
-					}
-
-					intrv := time.Duration(pingInterval.duration)
-					switch pingInterval.ofType {
-					case "hr":
-						time.Sleep(intrv * time.Hour)
-					case "min":
-						time.Sleep(intrv * time.Minute)
-					case "sec":
-						time.Sleep(intrv * time.Second)
-					default:
-						log.Fatalf("invalid interval-type for ping\n")
-						return
-					}
-				}
-			}()
-			return true
-		}
+		return handlePingStart(Configuration, pingServiceState)
 	case "stop":
 		Configuration.Config.UtilsConf.ServicesSignal.Ping = "passive"
 		_, e := Configuration.Write()
