c509fb44c99b254b6a13ab03d077ec6a0e1c2041
Flyznex <thuanpt.dev@gmail.com>
2019-10-06 16:23:12 +0700

    Simplify handlers.go
    * Simplify handlers.go
    
    Signed-off-by: flyznex <thuanpt.dev@gmail.com>
    
    * moved to new folder, unused urlHashMap
    
    Signed-off-by: flyznex <thuanpt.dev@gmail.com>
    
    * hotfix
    
    Signed-off-by: flyznex <thuanpt.dev@gmail.com>

--- src/lib/handlers.go
+++ src/lib/handlers.go
@@ -1,13 +1,10 @@
 package lib
 
 import (
-	"github.com/zairza-cetb/bench-routes/src/lib/filters"
-	"github.com/zairza-cetb/bench-routes/src/lib/modules/ping"
-	"github.com/zairza-cetb/bench-routes/src/lib/utils"
-	"github.com/zairza-cetb/bench-routes/tsdb"
 	"log"
-	"sync"
-	"time"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/handlers"
+	"github.com/zairza-cetb/bench-routes/src/lib/utils"
 )
 
 const (
@@ -46,69 +43,11 @@ func HandlerPingGeneral(signal string) bool {
 				panic(e)
 			}
 			go func() {
-				pingConfig := Configuration.Config.Routes
-				pingInterval := getInterval(Configuration.Config.Interval, "ping")
-				if pingInterval == (testInterval{}) {
-					log.Fatalf("interval not found in configuration file for ping")
-					return
-				}
-
-				urlStack := make([]string, 0)
-				urlHashMap := make(map[string]string)
-				for _, route := range pingConfig {
-					url := route.URL
-					exists := false
-
-					// maintain urls uniquely
-					for _, e := range urlStack {
-						if e == url {
-							exists = true
-						}
-					}
-					if !exists {
-						urlStack = append(urlStack, *filters.HTTPPingFilter(&url))
-						urlHashMap[url] = utils.GetHash(url)
-					}
-				}
-				i := 0
-				for {
-					i++
-					Configuration = Configuration.Refresh()
-
-					switch Configuration.Config.UtilsConf.ServicesSignal.Ping {
-					case "active":
-						var wg sync.WaitGroup
-						wg.Add(len(urlStack))
-						for u := range urlStack {
-							go ping.HandlePing(tsdb.GlobalPingChain, &urlStack[u], 10, urlHashMap[urlStack[u]], &wg, false)
-						}
-
-						wg.Wait()
-					case "passive":
-						// terminate the goroutine
-						log.Printf("terminating ping goroutine\n")
-						return
-					default:
-						log.Fatalf("invalid service-state value of ping\n")
-						return
-					}
-
-					intrv := time.Duration(pingInterval.duration)
-					switch pingInterval.ofType {
-					case "hr":
-						time.Sleep(intrv * time.Hour)
-					case "min":
-						time.Sleep(intrv * time.Minute)
-					case "sec":
-						time.Sleep(intrv * time.Second)
-					default:
-						log.Fatalf("invalid interval-type for ping\n")
-						return
-					}
-				}
+				handlers.HandlePingStart(Configuration, pingServiceState)
 			}()
 			return true
 		}
+		// return handlePingStart(Configuration, pingServiceState)
 	case "stop":
 		Configuration.Config.UtilsConf.ServicesSignal.Ping = "passive"
 		_, e := Configuration.Write()
@@ -120,24 +59,4 @@ func HandlerPingGeneral(signal string) bool {
 		log.Fatalf("invalid signal")
 	}
 	return false
-}
-
-type testInterval struct {
-	ofType   string
-	duration int64
-}
-
-func getInterval(intervals []utils.Interval, testName string) testInterval {
-	for _, intrv := range intervals {
-		if testName == intrv.Test {
-			return testInterval{
-				ofType:   intrv.Type,
-				duration: intrv.Duration,
-			}
-		}
-	}
-
-	// if the execution reaches this then it is an error as the interval was not found in the configuration file
-	log.Panicf("interval not found in the configuration file\n")
-	return testInterval{}
 }
