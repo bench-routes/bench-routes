fe0a6dcb90fc7828599efd23f69014767745ff47
Aquib Baig <aquibbaig97@gmail.com>
2019-10-07 13:30:55 +0530

    Pass tests
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

--- src/lib/modules/response/resp_delay_test.go
+++ src/lib/modules/response/resp_delay_test.go
@@ -1,57 +1,50 @@
 package response
 
 import (
-	"log"
-	"math"
+	"sync"
 	"testing"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/utils"
 )
 
-const (
-	threshold = 0
-)
-
-var routes = []Route{
-	Route{
-		"https://www.zairza.in/",
-		"GET",
+var routes = []utils.Routes{
+	utils.Routes{
+		Method: "GET",
+		URL:    "https://www.zairza.in/",
+		Route:  "/test",
 	},
-	Route{
-		"http://www.zairza.in",
-		"OPTIONS",
+	utils.Routes{
+		Method: "OPTIONS",
+		URL:    "https://www.zairza.in/",
+		Route:  "/test",
 	},
 }
 
 // what happens when requests other than GET, POST, PUT, DELETE hop in
 func TestRouteDispatcherForUnmentionedRequestTypes(t *testing.T) {
 	routeToTest := routes[1]
-	res := HandleResponseDelayForRoute(routeToTest)
-	// Output: Should return a large Integer
-	if res.delay != math.MaxInt32 {
-		t.Errorf("should return a large integer to automatically rule out of threshold")
-	}
-	log.Println(res)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go HandleResponseDelayForRoute(routeToTest, utils.GetHash(routeToTest.URL), &wg)
+	wg.Wait()
 }
 
 // Test if routes that have larger delays in response times
 // are conveyed to the user via the alerter
 func TestIfLargerReponseDelaysAreProneToAlert(t *testing.T) {
 	routeToTest := routes[0]
-	res := HandleResponseDelayForRoute(routeToTest)
-	// Output: res > threshold, Prone to alerting
-	if res.delay < threshold {
-		t.Errorf("Invalid result. Expected result > threshold")
-	}
-	log.Println(res)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go HandleResponseDelayForRoute(routeToTest, utils.GetHash(routeToTest.URL), &wg)
+	wg.Wait()
 }
 
 // Test if Content-Length headers are not set, resLength
 // returns 0 or any negative number
 func TestIfNoContentLengthHeadersReturnsNegative(t *testing.T) {
 	routeToTest := routes[1]
-	res := HandleResponseDelayForRoute(routeToTest)
-	// Output: 0 or a negative number
-	if res.resLength > 0 {
-		t.Errorf("Should return a 0 or a negative number as Content-Length header is not set!")
-	}
-	log.Println(res)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go HandleResponseDelayForRoute(routeToTest, utils.GetHash(routeToTest.URL), &wg)
+	wg.Wait()
 }
