0e586a7401c640a43af81062b2f0439911bffb33
Muskan Khedia <36980003+muskankhedia@users.noreply.github.com>
2019-10-18 15:58:29 +0530

    Integrating React with Electron (#4)
    * Initial commit from Create React App
    
    * setting up
    
    * removed files
    
    * added header
    
    * updated start scripts

---
+++ public/main.js
@@ -0,0 +1,45 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
+        minWidth: 800,
+        minHeight: 600,
+        center: true,
+        title: 'Bench-Routes - Mark your routes',
+        webPreferences: {
+          nodeIntegration: true
+        },
+        hasShadow: true,
+        autoHideMenuBar: true
+    })
+
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+    mainWindow.webContents.openDevTools();
+    
+    mainWindow.on('closed', () => mainWindow = null);
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
