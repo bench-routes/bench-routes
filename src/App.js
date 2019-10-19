ff6f622505d2d17e030def492d5d4017a182feb7
Harkishen-Singh <harkishensingh@hotmail.com>
2019-10-19 12:56:57 +0530

    sidebar ui added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Header from './components/layouts/Header';
+import Sidebar from './components/layouts/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <h1>
-          Welcome to Bench Routes
-        </h1>
-      </header>
+      <Sidebar className="sidebar" />
+      <div className="main">
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                bench-routes
+            </header>
+        );
+    }
+}
