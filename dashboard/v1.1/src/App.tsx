c1c0a94fa90103bbb94b7e9887b80bd529d5be34
Aquib Baig <aquibbaig97@gmail.com>
2020-03-07 14:05:09 +0530

    setup material-ui theme
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

--- dashboard/v1.1/src/App.tsx
+++ dashboard/v1.1/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import {
+  createMuiTheme,
+  ThemeProvider,
+  responsiveFontSizes
+} from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
+
+let theme = createMuiTheme({
+  palette: {
+    // Provides you with all
+    // shades of whites
+    primary: grey
+    // secondary: <Color>,
+  },
+  typography: {
+    fontFamily: ['Lato', 'Raleway'].join(','),
+    fontSize: 12
+  }
+});
+theme = responsiveFontSizes(theme);
 
 function App() {
-  return <div className="App">Bench-routes ui</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">Bench-routes ui</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
