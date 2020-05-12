065a71566768473fb5466dcb1ef765c1e8211a1b
Aquib Baig <aquibbaig97@gmail.com>
2020-05-12 11:45:27 +0530

    Extends ping and flood-ping support for mac and unix like systems
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

--- src/lib/filters/scraps/scrapPing.go
+++ src/lib/filters/scraps/scrapPing.go
@@ -1,9 +1,11 @@
 package scraps
 
 import (
-	"github.com/zairza-cetb/bench-routes/src/lib/utils"
 	"strconv"
 	"strings"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/logger"
+	"github.com/zairza-cetb/bench-routes/src/lib/utils"
 )
 
 func strToFloat64(s string) float64 {
@@ -20,7 +22,7 @@ func CLIPingScrap(s *string) (a *utils.TypePingScrap) {
 	l := len(arr)
 	if l > 2 {
 		words := strings.Split(arr[l-2], " ")
-		if words[0] == "rtt" {
+		if words[0] == "rtt" || words[0] == "round-trip" {
 			temp := strings.Split(words[3], "/")
 			a = &utils.TypePingScrap{
 				Min:  strToFloat64(temp[0]),
@@ -36,17 +38,40 @@ func CLIPingScrap(s *string) (a *utils.TypePingScrap) {
 // CLIFLoodPingScrap returns packet loss
 func CLIFLoodPingScrap(s *string) (a *utils.TypeFloodPingScrap) {
 	arr := strings.Split(*s, "\n")
-	sentence := arr[3]
-	words := strings.Split(sentence, ", ")
+	var (
+		// Packetloss line is of the form:
+		// "10 packets transmitted, 2 packets received, 80.0% packet loss"
+		packetLossLine string
+		// PingValues line is of the form:
+		// round-trip min/avg/max/stddev = 91.299/96.281/101.264/4.983 ms (Mac OS/Unix like systems)
+		// rtt min/avg/max/stddev = 91.299/96.281/101.264/4.983 ms (Linux)
+		pingValuesLine string
+	)
+	for _, line := range arr {
+		if strings.Contains(line, "packet loss") {
+			packetLossLine = line
+		} else if strings.Contains(line, "round-trip") {
+			pingValuesLine = line
+		} else if strings.Contains(line, "rtt") {
+			pingValuesLine = line
+		}
+	}
+	// This error handler is for an optional security
+	// if we could not find suitable parameters in the
+	// ping command's output.
+	if packetLossLine == "" || pingValuesLine == "" {
+		logger.Terminal("p", "couldn't find a suitable response which contains packet loss")
+	}
+	words := strings.Split(packetLossLine, ", ")
 	packetLoss := strings.Split(words[2], " ")[0]
 
-	pingValuesString := strings.Split(arr[4], " ")[3]
+	pingValuesString := strings.Split((strings.Split(pingValuesLine, "= ")[1]), " ms")[0]
 	pingValues := strings.Split(pingValuesString, "/")
 	a = &utils.TypeFloodPingScrap{
 		Min:        strToFloat64(pingValues[0]),
 		Avg:        strToFloat64(pingValues[1]),
 		Max:        strToFloat64(pingValues[2]),
-		Mdev:       strToFloat64(pingValues[3]),
+		Mdev:       strToFloat64(strings.TrimRight(pingValues[3], " ms")),
 		PacketLoss: strToFloat64(strings.TrimRight(packetLoss, "%")),
 	}
 
