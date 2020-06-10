99f1ca481b4ebd2645c575e3f4d418cde8536207
muskankhedia <muskan.khedia2000@gmail.com>
2020-06-10 23:24:44 +0530

    add page jitter test
    Signed-off-by: muskankhedia <muskan.khedia2000@gmail.com>

--- dashboard/v1.1/src/router/Navigation.tsx
+++ dashboard/v1.1/src/router/Navigation.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import FloodPing from '../pages/FloodPing';
-import Jitter from '../pages/Jitter';
+import JitterModule from '../pages/Jitter/JitterModule';
 import Monitoring from '../pages/Monitoring/Monitoring';
 import PingModule from '../pages/Ping/PingModule';
 import Settings from '../pages/Settings';
@@ -27,7 +27,7 @@ const Navigator: FC<NavigatorProps> = ({ updateLoader }) => {
         />
         <Route path="/ping" component={PingModule} />
         <Route path="/floodping" component={FloodPing} />
-        <Route path="/jitter" component={Jitter} />
+        <Route path="/jitter" component={JitterModule} />
         <Route path="/settings" component={Settings} />
       </Switch>
     </Router>
