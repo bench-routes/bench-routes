f7144c55c193027fa9905c85c7d9d6711526abc2
Aquib Baig <aquibbaig97@gmail.com>
2020-06-21 01:21:22 +0530

    dark mode feature (#270)
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

--- dashboard/v1.1/src/App.tsx
+++ dashboard/v1.1/src/App.tsx
@@ -1,32 +1,43 @@
-import { blue } from '@material-ui/core/colors';
+import { blue, grey } from '@material-ui/core/colors';
 import {
   createMuiTheme,
   responsiveFontSizes,
   ThemeProvider
 } from '@material-ui/core/styles';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './assets/bootstrap.min.css';
 import BaseLayout from './layouts/BaseLayout';
 
-let theme = createMuiTheme({
-  palette: {
-    // Provides you with all
-    // shades of whites
-    primary: blue
-    // secondary: <Color>,
-  },
-  typography: {
-    fontFamily: ['Lato', 'Raleway'].join(','),
-    fontSize: 12
-  }
-});
-theme = responsiveFontSizes(theme);
-
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  let theme = createMuiTheme({
+    palette: {
+      // Provides you with all
+      // shades of whites
+      type: darkMode ? 'dark' : 'light',
+      primary: blue
+      // secondary: <Color>,
+    },
+    typography: {
+      fontFamily: ['Lato', 'Raleway'].join(','),
+      fontSize: 12
+    },
+    overrides: {
+      MuiAppBar: {
+        colorPrimary: {
+          backgroundColor: darkMode ? grey[700] : blue[500]
+        }
+      }
+    }
+  });
+  theme = responsiveFontSizes(theme);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <ThemeProvider theme={theme}>
-      <BaseLayout />
+      <BaseLayout toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
     </ThemeProvider>
   );
 }
