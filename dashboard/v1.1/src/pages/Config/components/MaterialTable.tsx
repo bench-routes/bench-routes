0d29b684fce26a45c1c32d74b96259865cfbefee
Aquib Baig <aquibbaig97@gmail.com>
2020-06-21 01:32:55 +0530

    table component default row style
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

--- dashboard/v1.1/src/pages/Config/components/MaterialTable.tsx
+++ dashboard/v1.1/src/pages/Config/components/MaterialTable.tsx
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { tableIcons } from '../../../utils/tableIcons';
 
 import MaterialTable from 'material-table';
 
 const SearchTable = (props: any) => {
-  const [selectedRow, setSelectedRow] = useState(null);
-
   return (
     <MaterialTable
       icons={tableIcons}
       style={{ marginTop: '10vh' }}
-      onRowClick={(evt, selectedRow: any) =>
-        setSelectedRow(selectedRow?.tableData.id)
-      }
       options={{
         headerStyle: {
           fontSize: 20
-        },
-        rowStyle: rowData => ({
-          backgroundColor:
-            selectedRow === rowData.tableData.id ? '#EEE' : '#FFF'
-        })
+        }
       }}
       {...props}
     />
