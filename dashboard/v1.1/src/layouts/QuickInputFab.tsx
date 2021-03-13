4a489136c1d9dd8de1b9b515309ca6b95583f91d
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2021-03-13 17:59:13 +0530

    Moved quick-input from sidebar to FAB in monitoring Solves #257 (#340)

---
+++ dashboard/v1.1/src/layouts/QuickInputFab.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles, Tooltip, Fab } from '@material-ui/core';
+import { PostAdd as PostAddIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  inputFab: {
+    position: 'fixed',
+    right: '2rem',
+    bottom: '2rem'
+  }
+}));
+
+//Floating Action Button for Quick Input
+const QuickInputFab = () => {
+  const classes = useStyles();
+  return (
+    <Link to="/quick-input">
+      <Tooltip placement="top" title="Quick Route Input">
+        <Fab
+          className={classes.inputFab}
+          color="primary"
+          aria-label="quick-input"
+        >
+          <PostAddIcon />
+        </Fab>
+      </Tooltip>
+    </Link>
+  );
+};
+
+export default QuickInputFab;
