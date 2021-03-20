01682214d44b94e4ed837e855ced4643bf5f5219
shashwat <63119158+geforce6t@users.noreply.github.com>
2021-03-20 23:33:36 +0530

    Fix: jitter, ping page fetch error (#366)
    * [fix]: fix jitter, ping page fetch error
    * [refactor]: fetch route details single time
    * [fix]: handle the case of an invalid path with alert

--- dashboard/v1.1/src/pages/Jitter/JitterModule.tsx
+++ dashboard/v1.1/src/pages/Jitter/JitterModule.tsx
@@ -3,16 +3,16 @@ import { useFetch } from '../../utils/useFetch';
 import { service_states, HOST_IP } from '../../utils/types';
 import { Card, CardContent, TextField } from '@material-ui/core';
 import {
-  RoutesSummary,
   QueryResponse,
   chartData,
-  APIQueryResponse
+  APIQueryResponse,
+  APITimeSeriesResponse,
+  TimeSeries
 } from '../../utils/queryTypes';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Alert from '@material-ui/lab/Alert';
 import { formatTime } from '../../utils/brt';
 import Jitter from '../Monitoring/Jitter';
-import { filterUrl } from '../../utils/filterUrl';
 
 const format = (datas: QueryResponse) => {
   const jitter: chartData[] = [];
@@ -34,10 +34,12 @@ interface showChartsDataParam {
 }
 
 const JitterModule: FC<{}> = () => {
-  const [routesDetails, setRoutesDetails] = useState<RoutesSummary>();
-  const [value] = useState(routesDetails?.testServicesRoutes[0]);
+  const [hashRoutMap, SetHashRouteMap] = useState(new Map());
+  const [routesDetails, setRoutesDetails] = useState<string[]>();
+  const [value] = useState(routesDetails !== undefined ? routesDetails : '');
   const [inputValue, setInputValue] = useState('');
   const [showCharts, setShowCharts] = useState(false);
+  const [renderError, setRenderError] = useState(false);
   const [jitterData, setJitterData] = useState<showChartsDataParam>();
 
   const { response, error } = useFetch<service_states>(
@@ -45,27 +47,37 @@ const JitterModule: FC<{}> = () => {
   );
 
   useEffect(() => {
-    fetch(`${HOST_IP}/routes-summary`)
+    fetch(`${HOST_IP}/get-route-time-series`)
       .then(res => res.json())
-      .then((response: { status: string; data: RoutesSummary }) => {
-        setRoutesDetails(response.data);
+      .then((response: APITimeSeriesResponse) => {
+        let tempMap: Map<string, string> = new Map();
+        let tempArr: string[] = [];
+
+        response.data.forEach((item: TimeSeries) => {
+          tempArr.push(item.name);
+          tempMap.set(item.name, item.path.matrixName);
+        });
+
+        SetHashRouteMap(tempMap);
+        setRoutesDetails(tempArr);
       });
   }, []);
 
   async function getChartsData(v: string) {
-    let res = filterUrl(v);
-    res = res.substring(0, res.indexOf('/'));
-
     try {
       const response = await fetch(
-        `${HOST_IP}/query?timeSeriesPath=storage/jitter/chunk_jitter_${res}`
+        `${HOST_IP}/query?timeSeriesPath=storage/jitter/chunk_jitter_${hashRoutMap.get(
+          v
+        )}`
       );
       const matrix = (await response.json()) as APIQueryResponse;
       var formatdata = format(matrix.data);
       setJitterData(formatdata);
       setShowCharts(true);
+      setRenderError(false);
     } catch (e) {
       console.error(e);
+      setRenderError(true);
     }
   }
 
@@ -78,9 +90,7 @@ const JitterModule: FC<{}> = () => {
 
   // const states: service_states = response.data;
   const options =
-    routesDetails?.testServicesRoutes !== undefined
-      ? routesDetails.testServicesRoutes
-      : ['Please fill routes'];
+    routesDetails !== undefined ? routesDetails : ['Please fill routes'];
 
   return (
     <Card>
@@ -89,7 +99,7 @@ const JitterModule: FC<{}> = () => {
           <h4>Jitter</h4>
           <div style={{ float: 'right', marginTop: '-45px' }}>
             <Autocomplete
-              value={value}
+              value={value[0]}
               onChange={(event, newValue) => {}}
               inputValue={inputValue}
               onInputChange={(event, newInputValue) => {
@@ -112,9 +122,13 @@ const JitterModule: FC<{}> = () => {
         <br />
         <hr />
         <div>
-          {jitterData !== undefined && showCharts ? (
-            <Jitter value={jitterData.jitter} />
-          ) : null}
+          {!renderError ? (
+            jitterData !== undefined && showCharts ? (
+              <Jitter value={jitterData.jitter} />
+            ) : null
+          ) : (
+            <Alert severity="error">No data found</Alert>
+          )}
         </div>
       </CardContent>
     </Card>
