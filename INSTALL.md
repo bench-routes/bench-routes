eda6a4455a8973e1e097b22ffa755bdb3a365c0c
rishav4101 <rajkumarrishav4101@gmail.com>
2021-03-22 16:14:31 +0530

    Added: styling to monitoring searchbar

--- INSTALL.md
+++ INSTALL.md
@@ -20,4 +20,13 @@ environment.
 2. Run `docker build -t bench-routes .`
 3. Run `docker run -p 9990:9990 -it bench-routes`
 
+## Installation Instructions for Windows system using WSL
+
+1. Install [WSL](https://docs.microsoft.com/en-us/windows/wsl/install-win10) in your windows machine.
+2. Install Golang in your WSL distro.
+3. Set the gopath in WSL.
+4. Open the project in VScode WSL window.
+5. Install all dependencies via `go get -v -u ./...`.
+6. To start running, `make run` will start the service.
+
 Please feel free to open any [new-issue](https://github.com/bench-routes/bench-routes/issues/new/choose) in case you encounter with any issues while setting up the development environment.
--- dashboard/v1.1/src/pages/Monitoring/Matrix.tsx
+++ dashboard/v1.1/src/pages/Monitoring/Matrix.tsx
@@ -22,6 +22,7 @@ import { truncate } from '../../utils/stringManipulations';
 import { Badge } from 'reactstrap';
 import TablePagination from '@material-ui/core/TablePagination';
 import SearchBar from 'material-ui-search-bar';
+import { makeStyles } from '@material-ui/core/styles';
 
 type APIResponse<MatrixResponse> = { status: string; data: MatrixResponse };
 
@@ -38,6 +39,25 @@ interface ElementProps {
 }
 
 const Pad: FC<{}> = () => <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>;
+
+const useStyles = makeStyles({
+  searchBar: {
+    borderRadius: 0,
+    boxShadow: 'none',
+    borderBottom: '1px solid rgba(0, 0, 0, 0.23)',
+    backgroundColor: '#fff',
+    fontWeight: 600,
+    fontSize: 15,
+    color: '#000',
+    marginTop: '-47px',
+    maxWidth: 300,
+    float: 'right',
+    height: '30px',
+    '&:hover': {
+      borderBottom: '1px solid rgba(0, 0, 0, 1)'
+    }
+  }
+});
 
 const Element: FC<ElementProps> = ({
   timeSeriesPath,
@@ -244,6 +264,7 @@ const Matrix: FC<MatrixProps> = ({
   isMonitoringActive,
   showRouteDetails
 }) => {
+  const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -277,15 +298,8 @@ const Matrix: FC<MatrixProps> = ({
         onChange={searchVal => requestSearch(searchVal)}
         onCancelSearch={() => cancelSearch()}
         placeholder="Search here"
-        style={{
-          borderRadius: 0,
-          boxShadow: 'none',
-          borderBottom: '1px solid rgba(224, 224, 224, 1)',
-          backgroundColor: '#fafafa',
-          fontWeight: 600,
-          fontSize: 15,
-          color: '#000'
-        }}
+        className={classes.searchBar}
+        style={{}}
       />
       <TableContainer>
         <Table stickyHeader>
